12329907d3496dbf747fac02b46c95418f6b1abe
Jose Manuel <josmanual@users.noreply.github.com>
2017-12-17 17:55:21 +0100

    Updated to python 3

--- Raspberry Pi/updateIP.py
+++ Raspberry Pi/updateIP.py
@@ -1,19 +1,15 @@
-import httplib, urllib
+import urllib.request, urllib.parse
 import hashlib
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--url', help='Example: http://www.mypiweb.com', default='')
-parser.add_argument('--file', help='Example: updateIP.php', default='')
+parser.add_argument('--url', help='Example: http://www.mypiweb.com/updateIP.php', default='')
 parser.add_argument('--user', help='Your username', default='')
 parser.add_argument('--password', help='Your password', default='')
 args = parser.parse_args()
 
+data = {'USER' : args.user, 'PASS' : hashlib.md5(args.password.encode('utf-8')).hexdigest()}
+
 if (args.url != ""):
-	connection = httplib.HTTPConnection(args.url)
-	params = urllib.urlencode({"USER":args.user, "PASS":hashlib.md5(args.password).hexdigest()})
-	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-	connection.request("POST", "/" + args.file, params, headers)
-	response = connection.getresponse()
-	data = response.read()
-	connection.close()
+	data = bytes(urllib.parse.urlencode(data).encode())
+	handler = urllib.request.urlopen(args.url, data);
